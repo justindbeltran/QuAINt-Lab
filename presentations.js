85f1ee7cd86c713c7f23c5f6a2de92a19389a3fc
Justin <justinbeltran@protonmail.com>
2026-02-12 11:32:27 -0500

    Switching presentations to JS

---
+++ presentations.js
@@ -0,0 +1,39 @@
+// Warning only PDF's will render (browsers don't support PPT files)
+const presentations = [
+    {
+        "title": "The Public Option: A non-regulatory alternative to Network Neutrality",
+        "author": "QuAINt Lab",
+        "url": "https://uploads.quaint-lab.org/67b79136-f3b5-4917-b329-91d426d105fa.pdf",
+        "type": "PDF" 
+    },
+    {
+        "title": "Congestion Equilibrium for Differentiated Service Classes",
+        "author": "QuAINt Lab",
+        "url": "https://uploads.quaint-lab.org/2d58e8fc-69ba-47a1-868a-7289fb886fea.pdf",
+        "type": "PDF" 
+    },
+    {
+        "title": "How Green is IP-Telephony?",
+        "author": "QuAINt Lab",
+        "url": "https://uploads.quaint-lab.org/96d4f387-40f1-4f30-ab88-58c427f766e2.ppt",
+        "type": "PPT" // Will show fallback icon until converted to PDF
+    },
+    {
+        "title": "Incentivizing Peer-Assisted Services: A Fluid Shapley Value Approach",
+        "author": "DNA Lab",
+        "url": "https://uploads.quaint-lab.org/e20c20ef-bb9d-4849-ab4e-00b2ab88ef0e.pptx",
+        "type": "PPT"
+    },
+    {
+        "title": "Internet Economics: The use of Shapley value for ISP settlement",
+        "author": "DNA Lab",
+        "url": "https://uploads.quaint-lab.org/b8aad3df-a1dd-4e23-a5cc-6dfa67865a1e.ppt",
+        "type": "PPT"
+    },
+    {
+        "title": "On Cooperative Settlement Between Content, Transit and Eyeball Internet Service Providers",
+        "author": "DNA Lab",
+        "url": "https://uploads.quaint-lab.org/5ab6e3c6-ea73-420d-8dea-8a6662ab41cb.ppt",
+        "type": "PPT"
+    }
+];
